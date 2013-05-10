5a428461e91041836bd26447ceb46f661cdaaee8
Varun Ravishankar <vrdabomb@gmail.com>
2013-05-09 21:30:53 -0400

    Finish writing script to download all data.
    Signed-off-by: Varun Ravishankar <vrdabomb@gmail.com>

--- download_msd.sh
+++ download_msd.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env bash
 
-DATA=data/
+DATA=data
 MSD_SITE="http://labrosa.ee.columbia.edu/millionsong/sites/default/files/AdditionalFiles"
 
 mkdir -p $DATA
 
+## Download data for Lyrics Analysis
+
+wget -P $DATA $MSD_SITE/mxm_dataset_train.txt.zip
+wget -P $DATA $MSD_SITE/mxm_dataset_test.txt.zip
+
+unzip -d $DATA $DATA/mxm_dataset_train.txt.zip
+unzip -d $DATA $DATA/mxm_dataset_test.txt.zip
+
+cat $DATA/mxm_dataset_train.txt <(sed '1,18d' $DATA/mxm_dataset_test.txt) > $DATA/mxm1.txt
+
+rm $DATA/mxm_dataset_train.txt.zip
+rm $DATA/mxm_dataset_test.txt.zip
+rm $DATA/mxm_dataset_train.txt
+rm $DATA/mxm_dataset_test.txt
+
+## Download data for Duration, Volume, and Tempo Analysis
+
 # Download MSD Summary file for Tempo Histogram. 300 MB file.
-wget -p $DATA $MSD_SITE/msd_summary_file.h5
+wget -P $DATA $MSD_SITE/msd_summary_file.h5
 
 # Download Track Metadata SQLite DB for Duration Histogram. 750 MB file.
-wget -p $DATA $MSD_SITE/track_metadata.db
+wget -P $DATA $MSD_SITE/track_metadata.db
 
 # Download list of tracks with year information for tempo_v_time and volume_v_time
-wget -p $DATA $MSD_SITE/tracks_per_year.txt
+wget -P $DATA $MSD_SITE/tracks_per_year.txt
 
 unset DATA
 unset MSD_SITE
