7b1436e157cc39f6b66cbdc653cc2ac52290c520
Varun Ravishankar <vrdabomb@gmail.com>
2013-05-10 12:24:10 -0400

    Change download script to not download big files if they exist.
    Signed-off-by: Varun Ravishankar <vrdabomb@gmail.com>

--- download_msd.sh
+++ download_msd.sh
@@ -25,13 +25,13 @@ rm $DATA/mxm_dataset_test.txt
 ## Download data for Duration, Volume, and Tempo Analysis
 
 # Download MSD Summary file for Tempo Histogram. 300 MB file.
-wget -P $DATA $MSD_SITE/msd_summary_file.h5
+wget -nc -P $DATA $MSD_SITE/msd_summary_file.h5
 
 # Download Track Metadata SQLite DB for Duration Histogram. 750 MB file.
-wget -P $DATA $MSD_SITE/track_metadata.db
+wget -nc -P $DATA $MSD_SITE/track_metadata.db
 
 # Download list of tracks with year information for tempo_v_time and volume_v_time
-wget -P $DATA $MSD_SITE/tracks_per_year.txt
+wget -nc -P $DATA $MSD_SITE/tracks_per_year.txt
 
 unset DATA
 unset MSD_SITE
